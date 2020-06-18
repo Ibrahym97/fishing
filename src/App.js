7da82e16eca63ee57caa9c476377f7cd12baec59
Ibrahym97 <gasanovibragim693@gmail.com>
2020-06-18 05:15:36 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header"
+import Content from "./Content";
+import Footer from "./Footer";
+import Logo from "./Logo"
+
+function App() {
+
+    const appVersion = '0.2.321';
+    const menu = ['Salt Fishing','Fresh Fishing','Reels','On Ice'];
+    const footerMenu = ['Contact us','Help','F.A.Q.','Private Policy','About us', 'How to Donate']
+    return (
+        <div className="App">
+             <Logo/>
+            <Header verison={appVersion} menu={menu}/>
+            <Content/>
+           <Footer footerMenu={footerMenu}/>
+
+
+        </div>
+
+    );
+}
+
+export default App;
